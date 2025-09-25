a2fa6a250f1b52e746c883c9a5e627cced17d200
Natnael Getahun <natnael.getahun@suse.com>
2025-09-25 11:54:52 +0200

    Fix argument formatting in openssl command for key generation

--- spec/rmt/ssl/certificate_generator_spec.rb
+++ spec/rmt/ssl/certificate_generator_spec.rb
@@ -160,7 +160,7 @@ describe RMT::SSL::CertificateGenerator do
 
         expect_any_instance_of(Cheetah::DefaultRecorder).not_to receive(:record_stdin)
         expect(RMT::Execute).to receive(:on_target!).with(
-          'openssl', 'genpkey', '-algorithm RSA', '-aes256 -out', ssl_files[:ca_private_key], '-pkeyopt rsa_keygen_bits:', described_class::OPENSSL_KEY_BITS,
+          'openssl', 'genpkey', '-algorithm', 'RSA', '-aes256', '-out', ssl_files[:ca_private_key], '-pkeyopt', "rsa_keygen_bits:#{described_class::OPENSSL_KEY_BITS}",
           stdin: ca_password,
           logger: nil
         )
