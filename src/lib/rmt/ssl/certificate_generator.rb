yast/yast-rmt

commit 0b73535da927ffbf4304e598a81d0830c727eadb

Update OpenSSL command to use 'genpkey' for private key generation and bump rubocop versions

src/lib/rmt/ssl/certificate_generator.rb
@@ -98,7 +98,7 @@ class RMT::SSL::CertificateGenerator
       Yast::SCR.Write(Yast.path('.target.string'), @ssl_paths[:ca_config], config_generator.make_ca_config)
 
       RMT::Execute.on_target!(
-        'openssl', 'genrsa', '-aes256', '-out', @ssl_paths[:ca_private_key], '-pkeyopt', "rsa_keygen_bits:#{OPENSSL_KEY_BITS}",
+        'openssl', 'genpkey', '-algorithm', 'RSA', '-aes256', '-out', @ssl_paths[:ca_private_key], '-pkeyopt', "rsa_keygen_bits:#{OPENSSL_KEY_BITS}",
         stdin: ca_password,
         logger: nil # do not log in order to securely pass password
       )

spec/rmt/ssl/certificate_generator_spec.rb
@@ -160,7 +160,7 @@ describe RMT::SSL::CertificateGenerator do
 
         expect_any_instance_of(Cheetah::DefaultRecorder).not_to receive(:record_stdin)
         expect(RMT::Execute).to receive(:on_target!).with(
-          'openssl', 'genrsa', '-aes256', '-out', ssl_files[:ca_private_key], '-pkeyopt', "rsa_keygen_bits:#{described_class::OPENSSL_KEY_BITS}",
+          'openssl', 'genpkey', '-algorithm RSA', '-aes256 -out', ssl_files[:ca_private_key], '-pkeyopt rsa_keygen_bits:', described_class::OPENSSL_KEY_BITS,
           stdin: ca_password,
           logger: nil
         )

Gemfile
@@ -4,6 +4,6 @@ gem 'yast-rake', '~> 0.2.51'
 
 group :development do
   gem 'gettext'
-  gem 'rubocop', '~> 1.28.0'
-  gem 'rubocop-rspec', '~> 1.19.0'
+  gem 'rubocop', '~> 1.81.1'
+  gem 'rubocop-rspec', '~> 3.7.0'
 end

commit fe211100b441eaaa951aacf1116b04d7dc5a698f

Fix OpenSSL genpkey parameter formatting for SSL certificate generation
The OpenSSL genpkey command was failing with the error:
'Error setting rsa_keygen_bits: 2048 parameter'

This was caused by incorrect parameter formatting in the -pkeyopt
argument. The parameter 'rsa_keygen_bits: 2048' had a space after
the colon, but OpenSSL expects 'rsa_keygen_bits:2048' (no space).

This issue was introduced in commit 0b73535 when migrating from
genrsa to genpkey for FIPS compatibility.

Changes:
- Fixed the -pkeyopt parameter in certificate_generator.rb
- Updated the corresponding test expectation
- Added changelog entry

Testing:
Verified with OpenSSL command line:
  openssl genpkey -algorithm RSA -pass stdin -aes256 \
    -out /tmp/test.key -pkeyopt rsa_keygen_bits:2048
Successfully generates encrypted key with correct syntax.

Fixes: bsc#1266671
Related: bsc#1235462 (FIPS compatibility)

src/lib/rmt/ssl/certificate_generator.rb
@@ -99,7 +99,7 @@ class RMT::SSL::CertificateGenerator
 
       RMT::Execute.on_target!(
         'openssl', 'genpkey', '-algorithm', 'RSA', '-pass', 'stdin', '-aes256',
-        '-out', @ssl_paths[:ca_private_key], '-pkeyopt', "rsa_keygen_bits: #{OPENSSL_KEY_BITS}",
+        '-out', @ssl_paths[:ca_private_key], '-pkeyopt', "rsa_keygen_bits:#{OPENSSL_KEY_BITS}",
         stdin: ca_password,
         logger: nil # do not log in order to securely pass password
       )

spec/rmt/ssl/certificate_generator_spec.rb
@@ -161,7 +161,7 @@ describe RMT::SSL::CertificateGenerator do
         expect_any_instance_of(Cheetah::DefaultRecorder).not_to receive(:record_stdin)
         expect(RMT::Execute).to receive(:on_target!).with(
           'openssl', 'genpkey', '-algorithm', 'RSA', '-pass', 'stdin', '-aes256',
-          '-out', ssl_files[:ca_private_key], '-pkeyopt', "rsa_keygen_bits: #{described_class::OPENSSL_KEY_BITS}",
+          '-out', ssl_files[:ca_private_key], '-pkeyopt', "rsa_keygen_bits:#{described_class::OPENSSL_KEY_BITS}",
           stdin: ca_password,
           logger: nil
         )